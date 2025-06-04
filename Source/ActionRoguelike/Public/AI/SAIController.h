3935e7a87d12626f26131f0220cfdad3da7b6531
cakeus <caecus@gmail.com>
2025-06-03 23:47:16 -0700

    basic ai

---
+++ Source/ActionRoguelike/Public/AI/SAIController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API ASAIController : public AAIController
+{
+	GENERATED_BODY()
+
+	public:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	TObjectPtr<UBehaviorTree> BehaviorTree;
+};
